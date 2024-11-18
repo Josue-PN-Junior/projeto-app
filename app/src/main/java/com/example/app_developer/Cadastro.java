2e3a182285254ca69bfecfcd60d0d00278259162
Josue-PN-Junior <josue.junior.hub@gmail.com>
2024-11-18 20:00:06 -0300

    telas parte 1
    telas: Abertura, Login, Cadastro e sub-telas, não tem as partes funcionais de dados. Falta a tela Home.

--- app/src/main/java/com/example/app_developer/Cadastro.java
+++ app/src/main/java/com/example/app_developer/Cadastro.java
@@ -1,7 +1,7 @@
 package com.example.app_developer;
 
-import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
@@ -10,17 +10,39 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class Cadastro extends AppCompatActivity implements View.OnClickListener{
+    private PopUP PopUP3;
+    Button bt_CadONGS, bt_CadPopUp;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_cadastro);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // classe
+        PopUP3 = new PopUP(this);
+
+        // indetificando
+        bt_CadONGS = (Button) findViewById(R.id.bt_CadONGS);
+        bt_CadPopUp = (Button) findViewById(R.id.bt_CadPopUp);
+
+
+        // Ouve
+        bt_CadPopUp.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.bt_CadPopUp) {
+            PopUP3.showPop(this);
+        }
     }
 }
--- app/src/main/java/com/example/app_developer/Login.java
+++ app/src/main/java/com/example/app_developer/Login.java
@@ -1,8 +1,9 @@
 package com.example.app_developer;
 
-import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,17 +11,32 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class Login extends AppCompatActivity implements View.OnClickListener {
+
+    TextView login_recSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        login_recSenha = (TextView) findViewById(R.id.login_recSenha);
+
+        login_recSenha.setOnClickListener( this );
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.login_recSenha) {
+            Intent telaRec = new Intent(this, RecuperarSenha.class);
+            startActivity(telaRec);
+        }
     }
 }
